b18018e52bcc60d97c8a4bcf768777401978d78d
var4pro <var4pro@gmail.com>
2026-08-16 00:13:59 +0300

    v16 added necessary extern

--- include/Allocator.h
+++ include/Allocator.h
@@ -6,5 +6,6 @@ static constexpr INTN GENERAL_ARRAY_MAX_LEN = 1024;
 extern VOID* Var4alloc(UINTN size);
 
 extern void cleanup_var4free(void* pp);
+extern void cleanup_zero(void* pp);
 #define AUTO_FREE __attribute__((cleanup(cleanup_var4free)))
 #define AUTO_SET_TO_ZERO __attribute__((cleanup(cleanup_var4free)))
